206d6ecd071c352da348a1c7d8464cad231763f1
Svetlana <roksana17.1.kz@gmail.com>
2024-01-11 23:11:08 +0600

    2.6 task

--- bbp.js
+++ bbp.js
@@ -1,5 +1,18 @@
-const firstName = 'Svetlana'
-let age = '25'
-age = '28'
-const city = 'Karaganda'
-console.log('Hello', firstName, age, city, hobby)
+// const firstName = 'Svetlana'
+// let age = '25'
+// age = '28'
+// const city = 'Karaganda'
+// console.log('Hello', firstName, age, city, hobby);
+
+
+
+
+
+
+
+ let i = 0;
+ do {
+  console.log(i);
+  getAllServicePrices()++;
+ } while (i > 70000);
+
